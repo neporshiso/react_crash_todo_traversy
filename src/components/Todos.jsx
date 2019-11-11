3e2e69f4b77c997c98d9c8386b84fbf3f0911926
Nep T. Orshiso <nep.orshiso@gmail.com>
2019-11-10 20:19:36 -0500

    initial commit

---
+++ src/components/Todos.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class Todos extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>App</h1>
+            </div>
+        );
+    }
+}
+
+export default Todos;
